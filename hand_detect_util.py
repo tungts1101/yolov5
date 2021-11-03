d2cedb7dd1a4a74d639ab4f53c9c92052b0f97e4
Tran Son Tung <tungts1101@gmail.com>
2021-11-03 17:33:53 +0700

    Update min & max bb

--- hand_detect_util.py
+++ hand_detect_util.py
@@ -31,13 +31,18 @@ from utils.plots import colors, plot_one_box
 from collections import Counter
 from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
+import trimesh
 
 subject_names = ["Subject_1", "Subject_2", "Subject_3", "Subject_4", "Subject_5", "Subject_6"]
-gesture_names = ['charge_cell_phone','clean_glasses','close_juice_bottle','close_liquid_soap','close_milk','close_peanut_butter','drink_mug','flip_pages','flip_sponge', 'give_card',
-'give_coin','handshake','high_five','light_candle','open_juice_bottle','open_letter','open_liquid_soap','open_milk','open_peanut_butter','open_soda_can','open_wallet','pour_juice_bottle'
-'pour_liquid_soap','pour_milk','pour_wine','prick','put_salt','put_sugar','put_tea_bag','read_letter','receive_coin', 'scoop_spoon','scratch_sponge','sprinkle','squeeze_paper',
-'squeeze_sponge','stir','take_letter_from_enveloppe','tear_paper','toast_wine','unfold_glasses','use_calculator','use_flash','wash_sponge','write']
+# subject_names = ["Subject_1"]
+# gesture_names = ['charge_cell_phone','clean_glasses','close_juice_bottle','close_liquid_soap','close_milk','close_peanut_butter','drink_mug','flip_pages','flip_sponge', 'give_card',
+# 'give_coin','handshake','high_five','light_candle','open_juice_bottle','open_letter','open_liquid_soap','open_milk','open_peanut_butter','open_soda_can','open_wallet','pour_juice_bottle'
+# 'pour_liquid_soap','pour_milk','pour_wine','prick','put_salt','put_sugar','put_tea_bag','read_letter','receive_coin', 'scoop_spoon','scratch_sponge','sprinkle','squeeze_paper',
+# 'squeeze_sponge','stir','take_letter_from_enveloppe','tear_paper','toast_wine','unfold_glasses','use_calculator','use_flash','wash_sponge','write']
 #gesture_names = ['put_salt','use_calculator','take_letter_from_enveloppe','open_juice_bottle','open_juice_bottle','open_letter','open_liquid_soap','open_milk','open_peanut_butter','open_soda_can','open_wallet']
+gesture_names = ['close_juice_bottle', 'close_liquid_soap', 'close_milk', 'open_juice_bottle', 'open_liquid_soap', 
+'open_milk', 'pour_juice_bottle', 'pour_liquid_soap', 'pour_milk', 'put_salt']
+# gesture_names = ['put_salt']
 
 def generate_point_cloud_from_depth(depth_val,img_width,img_height,depth_threshold=550,is_visualize=False, voxel_size=3.0, dbscan_eps=8.0,use_voxel_downsample=True):
     # ===== crop all parts too far from camera =====
@@ -170,7 +175,8 @@ def generate_point_cloud_from_depth(depth_val,img_width,img_height,depth_thresho
     if is_visualize:
         o3d.visualization.draw_geometries([pcd])
 
-    return (pcd, pcd.get_oriented_bounding_box())
+    # return (pcd, pcd.get_oriented_bounding_box())
+    return (pcd, o3d.geometry.OrientedBoundingBox.create_from_points(o3d.utility.Vector3dVector(np.array(pcd.points))))
 
 @torch.no_grad()
 def run(weights='yolov5s.pt',  # model.pt path(s)
@@ -209,13 +215,69 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     video_dir = os.path.join(source, 'Video_files')
     hand_annotation = os.path.join(source, 'Hand_pose_annotation_v1')
 
+    cam_extr = np.array([[0.999988496304, -0.00468848412856, 0.000982563360594, 25.7],
+                     [0.00469115935266, 0.999985218048, -0.00273845880292, 1.22],
+                     [-0.000969709653873, 0.00274303671904, 0.99999576807, 3.902],
+                     [0, 0, 0, 1]])
+    
+    obj_trans_root = os.path.join(source, 'Object_6D_pose_annotation_v1_1')
+    obj_root = os.path.join(source, 'Object_models')
+
+    obj_map_with_action = {
+        'close_juice_bottle': 'juice_bottle',
+        'close_liquid_soap': 'liquid_soap',
+        'close_milk': 'milk', 
+        'open_juice_bottle': 'juice_bottle', 
+        'open_liquid_soap': 'liquid_soap', 
+        'open_milk': 'milk', 
+        'pour_juice_bottle': 'juice_bottle', 
+        'pour_liquid_soap': 'liquid_soap', 
+        'pour_milk': 'milk', 
+        'put_salt': 'salt'
+    }
+
+    def load_objects(obj_root):
+        object_names = ['juice_bottle', 'liquid_soap', 'milk', 'salt']
+        all_models = {}
+        for obj_name in object_names:
+            obj_path = os.path.join(obj_root, '{}_model'.format(obj_name),
+                                    '{}_model.ply'.format(obj_name))
+            mesh = trimesh.load(obj_path)
+            all_models[obj_name] = mesh
+        return all_models
+
+    def get_obj_transform(sample, obj_root):
+        seq_path = os.path.join(obj_root, sample['subject'], sample['action_name'],
+                                sample['seq_idx'], 'object_pose.txt')
+        with open(seq_path, 'r') as seq_f:
+            raw_lines = seq_f.readlines()
+        raw_line = raw_lines[sample['frame_idx']]
+        line = raw_line.strip().split(' ')
+        trans_matrix = np.array(line[1:]).astype(np.float32)
+        trans_matrix = trans_matrix.reshape(4, 4).transpose()
+        # print('Loading obj transform from {}'.format(seq_path))
+        return trans_matrix
+
+    object_infos = load_objects(obj_root)
+
+    def cam_coords(world_coord):
+        if world_coord.ndim == 1:
+            world_coord_mat = np.concatenate([world_coord, np.ones(1)])
+            return cam_extr.dot(world_coord_mat.transpose()).transpose()[:3].astype(np.float32)
+        else:
+            B, S = world_coord.shape
+            if S == 3:
+                world_coord_mat = np.concatenate([world_coord, np.ones([B, 1])], 1)
+
+            return cam_extr.dot(world_coord_mat.transpose()).transpose()[:, :3].astype(np.float32)
+
     for subject in subject_names:
         print("Subject: {}".format(subject))
         time1 = time.time()
         for gesture in gesture_names:
-            print("Gesture: {}".format(gesture))
             try:
                 if not os.path.exists(os.path.join(video_dir, subject, gesture)): continue
+                print("Gesture: {}".format(gesture))
                 for seq_idx in os.listdir(os.path.join(video_dir, subject, gesture)):
                     if not seq_idx.isnumeric(): continue
                     if not os.path.exists(os.path.join(video_dir, subject, gesture, seq_idx)): continue
@@ -240,6 +302,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                     volume_rotate = np.zeros((frame_num, 3, 3)).astype(np.float32) # rotation matrix
                     bound_obb = np.zeros((frame_num, 2, 3)).astype(np.float32) # min bound & max bound of rotation matrix
                     gt_xyz = np.zeros((frame_num, 63)).astype(np.float32)
+                    obj_xyz = np.zeros((frame_num, 8, 3)).astype(np.float32) # coordinate of obj
                     valid = [False for _ in range(frame_num)]
     
                     image_dir = os.path.join(video_dir, subject, gesture, seq_idx, 'color')
@@ -296,29 +359,78 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     
                                     valid[frame_idx] = True
                                     volume_rotate[frame_idx] = obb.R
-    
-                                    min_bound = obb.get_min_bound()
-                                    max_bound = obb.get_max_bound()
-                                    bound_obb[frame_idx] = np.asarray([min_bound, max_bound])
-    
-                                    # rotate & normalize point cloud
+
                                     pcd.rotate(obb.R.transpose(), obb.get_center())
                                     pcd_points = np.asarray(pcd.points)
-                                    obb_len = (max_bound - min_bound)
+                                    min_bound = np.min(np.array(pcd_points), axis=0)
+                                    max_bound = np.max(np.array(pcd_points), axis=0)
+                                    bound_obb[frame_idx] = np.asarray([min_bound, max_bound])
+                                    obb_len = max_bound - min_bound + 1e-6
+
+                                    # min_bound_camcoords = cam_coords(min_bound)
+                                    # max_bound_camcoords = cam_coords(max_bound)
+                                    # bound_obb[frame_idx] = np.asarray([min_bound_camcoords, max_bound_camcoords])
+                                    # obb_len = max_bound_camcoords - min_bound_camcoords + 1e-6
+
+                                    pcd_normals = np.asarray(pcd.normals)
+                                    # pcd_points_camcoords = cam_coords(pcd_points)
+
+                                    # pcd.points = pcd_points_camcoords
+                                    # pcd.normals = pcd_normal_camcoords
+                                    # pcd_camcoords = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pcd_points))
+                                    # pcd_camcoords.normals = o3d.utility.Vector3dVector(pcd_normals)
+                                    # o3d.visualization.draw_geometries([pcd_camcoords])
+
                                     pcd_points = (pcd_points - min_bound) / obb_len
-                                    points[frame_idx] = np.concatenate((pcd_points, np.asarray(pcd.normals)), axis=1)
+                                    # pcd_points_camcoords = (pcd_points_camcoords - min_bound_camcoords) / obb_len
+                                    # print(np.all(pcd_points <= 1.0))
+                                    # print(np.all(pcd_points >= -1.0))
+                                    # print(np.all(pcd_normals <= 1.0))
+                                    # print(np.all(pcd_normals >= -1.0))
+
+                                    points[frame_idx] = np.concatenate((pcd_points, pcd_normals), axis=1)
     
                                     # rotate & normalize ground truth
                                     i_gt_xyz = gt_ws[frame_idx].reshape((21, 3))
                                     i_gt_xyz = np.matmul(i_gt_xyz, obb.R.transpose())
                                     i_gt_xyz = (i_gt_xyz - min_bound) / obb_len
                                     gt_xyz[frame_idx] = i_gt_xyz.flatten()
+                                    # i_gt_xyz_camcoords = cam_coords(i_gt_xyz)
+                                    # i_gt_xyz_camcoords = (i_gt_xyz_camcoords - min_bound_camcoords) / obb_len
+                                    # gt_xyz[frame_idx] = i_gt_xyz_camcoords.flatten()
+
+                                    # calculate obj pose
+                                    sample = {
+                                        'subject': subject,
+                                        'action_name': gesture,
+                                        'seq_idx': seq_idx,
+                                        'frame_idx': frame_idx
+                                    }
+
+                                    obj_trans = get_obj_transform(sample, obj_trans_root)
+                                    mesh = object_infos[obj_map_with_action[gesture]]
+                                    verts = np.array(mesh.bounding_box_oriented.vertices) * 1000
+
+                                    hom_verts = np.concatenate([verts, np.ones([verts.shape[0], 1])], axis=1)
+                                    verts_trans = obj_trans.dot(hom_verts.T).T
+                                    verts_trans = verts_trans[:, :-1]
+
+                                    # rotate & normalize
+                                    verts_trans = np.matmul(verts_trans, obb.R.transpose())
+                                    verts_trans = (verts_trans - min_bound) / obb_len
+                                    obj_xyz[frame_idx] = verts_trans
+                                    # verts_camcoords = cam_coords(verts_trans)
+                                    # verts_camcoords = (verts_camcoords - min_bound_camcoords) / obb_len
+                                    # print(np.all(verts_camcoords <= 1.0))
+                                    # print(np.all(verts_camcoords >= -1.0))
+                                    # obj_xyz[frame_idx] = verts_camcoords
 
                     np.save(os.path.join(save_seq_path, 'points.npy'), points)
                     np.save(os.path.join(save_seq_path, 'volume_rotate.npy'), volume_rotate)
                     np.save(os.path.join(save_seq_path, 'bound_obb.npy'), bound_obb)
                     np.save(os.path.join(save_seq_path, 'gt_xyz.npy'), gt_xyz)
                     np.save(os.path.join(save_seq_path, 'valid.npy'), valid)
+                    np.save(os.path.join(save_seq_path, 'obj_xyz.npy'), obj_xyz)
             except Exception as e:
                 print(e)
 
@@ -326,12 +438,12 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--weights', '-w', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', '-s', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--device', '-d', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
